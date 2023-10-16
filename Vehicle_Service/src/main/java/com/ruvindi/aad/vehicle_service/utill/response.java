9faf94980cd3ee11fa9b3cf7100e56251d60d2c3
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-16 22:01:47 +0530

    update

--- Vehicle_Service/src/main/java/com/ruvindi/aad/vehicle_service/utill/response.java
+++ Vehicle_Service/src/main/java/com/ruvindi/aad/vehicle_service/utill/response.java
@@ -1,4 +1,17 @@
 package com.ruvindi.aad.vehicle_service.utill;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class response {
+    private String massage;
+    private String state;
+    private Object data;
+
 }
