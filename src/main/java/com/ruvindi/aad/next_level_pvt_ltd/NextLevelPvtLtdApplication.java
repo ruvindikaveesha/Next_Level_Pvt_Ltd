1bf28afff8478b49ff0f3276ef5c25d974fcfcd2
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-16 14:27:06 +0530

    Initial commit

---
+++ src/main/java/com/ruvindi/aad/next_level_pvt_ltd/NextLevelPvtLtdApplication.java
@@ -0,0 +1,13 @@
+package com.ruvindi.aad.next_level_pvt_ltd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextLevelPvtLtdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextLevelPvtLtdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruvindi/aad/next_level_pvt_ltd/NextLevelPvtLtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruvindi.aad.next_level_pvt_ltd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextLevelPvtLtdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
