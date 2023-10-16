6edd53a31d9b8d53a42ba99f4ce5df0fe95de906
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-16 21:30:12 +0530

    update

--- Vehicle_Service/src/main/java/com/ruvindi/aad/vehicle_service/service/Impl/VehicleService.java
+++ Vehicle_Service/src/main/java/com/ruvindi/aad/vehicle_service/service/Impl/VehicleService.java
@@ -1,4 +1,12 @@
 package com.ruvindi.aad.vehicle_service.service.Impl;
 
+import com.ruvindi.aad.vehicle_service.entity.Vehicle;
+
+import java.util.List;
+
 public interface VehicleService {
+    Vehicle addVehicle(Vehicle vehicle);
+    List<Vehicle> fetchAllVehicle();
+    boolean checkExistsVehicle(String regNmuber,String brand);
+
 }
