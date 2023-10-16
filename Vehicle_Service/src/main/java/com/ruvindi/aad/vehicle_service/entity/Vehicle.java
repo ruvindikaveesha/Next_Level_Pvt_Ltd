46089eb5ae763b3583b6b2c3c6bc981152c47293
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-16 20:26:20 +0530

    update

--- Vehicle_Service/src/main/java/com/ruvindi/aad/vehicle_service/entity/Vehicle.java
+++ Vehicle_Service/src/main/java/com/ruvindi/aad/vehicle_service/entity/Vehicle.java
@@ -1,4 +1,29 @@
 package com.ruvindi.aad.vehicle_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "vehicle_details")
+
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String regNumber;
+    private String category;
+    private String brand;
+    private String fuelUsage;
+    private String fuelType;
+    private String frontImgUrl;
+    private String rearImgUrl;
+    private String vehicleType;
+    private String transmissionType;
+    private int seatCapacity;
+    private boolean hybrid;
+
 }
