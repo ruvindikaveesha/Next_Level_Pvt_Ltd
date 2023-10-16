6b268b9ab3a749959a720ac8985f05d4799de101
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-16 14:41:40 +0530

    update

---
+++ Vehicle_Service/src/test/java/com/ruvindi/aad/vehicle_service/VehicleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruvindi.aad.vehicle_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
