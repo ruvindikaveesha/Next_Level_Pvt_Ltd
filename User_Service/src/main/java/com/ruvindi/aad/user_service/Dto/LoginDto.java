9b792991eb7e75976c3461ab435f8b7cfc89545e
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-16 18:37:20 +0530

    update

---
+++ User_Service/src/main/java/com/ruvindi/aad/user_service/Dto/LoginDto.java
@@ -0,0 +1,15 @@
+package com.ruvindi.aad.user_service.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class LoginDto {
+    private String userName;
+    private String password;
+
+}
