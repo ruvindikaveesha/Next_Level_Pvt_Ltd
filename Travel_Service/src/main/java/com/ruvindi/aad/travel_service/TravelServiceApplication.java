498fca20e547dc2de01e337ce85139e36589cb77
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-16 14:34:04 +0530

    update

---
+++ Travel_Service/src/main/java/com/ruvindi/aad/travel_service/TravelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ruvindi.aad.travel_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelServiceApplication.class, args);
+    }
+
+}
---
+++ Travel_Service/src/test/java/com/ruvindi/aad/travel_service/TravelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruvindi.aad.travel_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
