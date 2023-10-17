dcfcaf96e2b4b0af00a4d634e3f8ad0328412618
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-17 14:21:01 +0530

    update

--- Travel_Service/src/main/java/com/ruvindi/aad/travel_service/service/TravelPackageService.java
+++ Travel_Service/src/main/java/com/ruvindi/aad/travel_service/service/TravelPackageService.java
@@ -1,4 +1,15 @@
 package com.ruvindi.aad.travel_service.service;
 
+import com.ruvindi.aad.travel_service.entity.TravelPackage;
+
+import java.util.List;
+
 public interface TravelPackageService {
+    void addTravelPackage(TravelPackage travelPackage);
+
+    TravelPackage updateTravelPackage(TravelPackage travelPackage);
+
+    void deletePackage(Integer id);
+
+    List<TravelPackage> fetchAllPackage();
 }
