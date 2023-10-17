8b64b04c7f1f9c05a373ce949674d590588457c8
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-17 20:16:48 +0530

    update

--- Travel_Service/src/main/java/com/ruvindi/aad/travel_service/controller/TravelPackageController.java
+++ Travel_Service/src/main/java/com/ruvindi/aad/travel_service/controller/TravelPackageController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("*")
 @RequestMapping(value = "/package")
 
 public class TravelPackageController {
