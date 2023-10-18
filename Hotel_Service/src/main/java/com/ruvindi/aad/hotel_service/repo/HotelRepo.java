4123a4a77539ecccab322042e97e0887e3ff3bc0
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 12:06:52 +0530

    update

--- Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/repo/HotelRepo.java
+++ Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/repo/HotelRepo.java
@@ -1,4 +1,8 @@
 package com.ruvindi.aad.hotel_service.repo;
 
-public interface HotelRepo {
+import com.ruvindi.aad.hotel_service.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepo extends JpaRepository<Hotel,Integer> {
+
 }
