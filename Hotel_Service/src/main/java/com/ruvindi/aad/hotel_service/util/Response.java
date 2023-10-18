8d25a60469891a2246379c93b25975c1a28e4ead
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 12:15:00 +0530

    update

--- Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/util/Response.java
+++ Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/util/Response.java
@@ -1,4 +1,17 @@
 package com.ruvindi.aad.hotel_service.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class Response {
+    private String state;
+    private String massage;
+    private Object data;
+
 }
