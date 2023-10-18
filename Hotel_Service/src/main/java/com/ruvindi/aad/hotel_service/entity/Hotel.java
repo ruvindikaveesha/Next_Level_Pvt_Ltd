b5fe71fe9241d075570f4a0d2951b909a8d99c05
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 12:05:06 +0530

    update

--- Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/entity/Hotel.java
+++ Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/entity/Hotel.java
@@ -1,4 +1,28 @@
 package com.ruvindi.aad.hotel_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "hotel_details")
+
 public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+    private String contact;
+    private String location;
+    private String hotelCategory;
+    private String googleMapCoordinates;
+    private boolean petAreAllowed;
+    private double hotelFee;
+    private String cancellation_criteria;
+
 }
