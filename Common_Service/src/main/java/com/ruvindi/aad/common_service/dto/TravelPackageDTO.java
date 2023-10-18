83c27d1e4f949dc7f0a196242736884bd3566c32
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 22:25:50 +0530

    update

--- Common_Service/src/main/java/com/ruvindi/aad/common_service/dto/TravelPackageDTO.java
+++ Common_Service/src/main/java/com/ruvindi/aad/common_service/dto/TravelPackageDTO.java
@@ -1,4 +1,15 @@
 package com.ruvindi.aad.common_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TravelPackageDTO {
+    private int id;
+    private String packageCategory;
+    private int starRate;
+    private String description;
 }
