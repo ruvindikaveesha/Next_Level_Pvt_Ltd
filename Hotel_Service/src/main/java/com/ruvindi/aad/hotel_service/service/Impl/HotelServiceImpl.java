6ad8cc9a21e514f53abc29b9c6747e181adefd2d
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 13:22:17 +0530

    update

--- Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/service/Impl/HotelServiceImpl.java
+++ Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/service/Impl/HotelServiceImpl.java
@@ -1,6 +1,27 @@
 package com.ruvindi.aad.hotel_service.service.Impl;
 
+import com.ruvindi.aad.hotel_service.entity.Hotel;
+import com.ruvindi.aad.hotel_service.service.HotelService;
+import org.springframework.stereotype.Service;
 
-public class HotelServiceImpl {
+import java.util.List;
 
+@Service
+public class HotelServiceImpl implements HotelService {
+
+
+    @Override
+    public Hotel addHotel(Hotel hotel) {
+        return null;
+    }
+
+    @Override
+    public List<Hotel> fetchAllHotel() {
+        return null;
+    }
+
+    @Override
+    public boolean checkExistsHotel(Integer hotelId, String name) {
+        return false;
+    }
 }
