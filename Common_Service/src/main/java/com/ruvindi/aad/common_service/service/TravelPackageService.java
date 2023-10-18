9282da4939ef4b2c082a0ded6e8710b3f4f98ed6
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 23:09:17 +0530

    update

--- Common_Service/src/main/java/com/ruvindi/aad/common_service/service/TravelPackageService.java
+++ Common_Service/src/main/java/com/ruvindi/aad/common_service/service/TravelPackageService.java
@@ -1,4 +1,19 @@
 package com.ruvindi.aad.common_service.service;
 
+import com.ruvindi.aad.common_service.dto.TravelPackageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
 public class TravelPackageService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List<TravelPackageDTO> getAllTravelPackages(){
+        return restTemplate.getForObject("http://TRAVEL-SERVICE/packages/get", List.class);
+    }
+
 }
