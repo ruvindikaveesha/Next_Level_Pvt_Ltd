edcb7444943a9349308fe8eb032c94f3302ecff3
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 22:10:20 +0530

    update

---
+++ Commen_Service/src/test/java/com/ruvindi/aad/commen_service/CommenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruvindi.aad.commen_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommenServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
