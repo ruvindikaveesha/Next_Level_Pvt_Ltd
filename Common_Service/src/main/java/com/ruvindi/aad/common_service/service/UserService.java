867bf4eff138d25eed97bd6fc2d7f0b3b24d645c
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 22:22:49 +0530

    update

--- Common_Service/src/main/java/com/ruvindi/aad/common_service/service/UserService.java
+++ Common_Service/src/main/java/com/ruvindi/aad/common_service/service/UserService.java
@@ -1,4 +1,22 @@
 package com.ruvindi.aad.common_service.service;
 
+import com.ruvindi.aad.common_service.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class UserService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Boolean checkLoing(String userName,String password){
+        return restTemplate.getForObject("http://USER-SERVICE/user/check?userName="+ userName +"&password=" + password, Boolean.class)
+    }
+
+    public ResponseEntity<UserDTO> createUser(UserDTO userDTO){
+        return restTemplate.postForEntity("http://USER-SERVICE/user/add",userDTO,UserDTO.class)
+    }
+
 }
