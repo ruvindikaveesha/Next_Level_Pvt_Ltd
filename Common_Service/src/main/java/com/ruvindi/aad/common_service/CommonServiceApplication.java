39662c66e86eafd350a98a827f59961e312b0a8b
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 22:37:37 +0530

    update

--- Common_Service/src/main/java/com/ruvindi/aad/common_service/CommonServiceApplication.java
+++ Common_Service/src/main/java/com/ruvindi/aad/common_service/CommonServiceApplication.java
@@ -2,6 +2,9 @@ package com.ruvindi.aad.common_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CommonServiceApplication {
@@ -10,4 +13,9 @@ public class CommonServiceApplication {
         SpringApplication.run(CommonServiceApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
