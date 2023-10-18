2a9cbbe21b7f7975121b5299b91520088a0e2ff1
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 16:19:21 +0530

    update

--- Driver_Service/src/main/java/com/ruvindi/aad/driver_service/entity/Driver.java
+++ Driver_Service/src/main/java/com/ruvindi/aad/driver_service/entity/Driver.java
@@ -1,4 +1,23 @@
 package com.ruvindi.aad.driver_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "driver_details")
 public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Integer id;
+    private String name;
+    private String address;
+    private String contactNum;
+    private String drivingLicenseFrontImg;
+    private String drivingLicenseRearImg;
 }
