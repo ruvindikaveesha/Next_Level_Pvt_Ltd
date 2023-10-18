222dfdca1289b836222e96409561ad0debb08113
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 19:32:04 +0530

    update

--- Driver_Service/src/main/java/com/ruvindi/aad/driver_service/service/Impl/DriverServiceImpl.java
+++ Driver_Service/src/main/java/com/ruvindi/aad/driver_service/service/Impl/DriverServiceImpl.java
@@ -1,4 +1,30 @@
 package com.ruvindi.aad.driver_service.service.Impl;
 
-public class DriverServiceImpl {
+import com.ruvindi.aad.driver_service.entity.Driver;
+import com.ruvindi.aad.driver_service.service.DriverService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public void addDriver(Driver driver) {
+
+    }
+
+    @Override
+    public List<Driver> fetchAllDriver() {
+        return null;
+    }
+
+    @Override
+    public boolean checkExistsDriver(Integer driverId, String name) {
+        return false;
+    }
+
+    @Override
+    public void updateDriver(Driver driver) {
+
+    }
 }
