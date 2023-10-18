ba988d3465560a6c438c11421b28b7ece5a67c82
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 12:09:31 +0530

    update

--- Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/service/HotelService.java
+++ Hotel_Service/src/main/java/com/ruvindi/aad/hotel_service/service/HotelService.java
@@ -1,4 +1,11 @@
 package com.ruvindi.aad.hotel_service.service;
 
+import com.ruvindi.aad.hotel_service.entity.Hotel;
+
+import java.util.List;
+
 public interface HotelService {
+    Hotel addHotela(Hotel hotel);
+    List<Hotel> fetchAllHotel();
+    boolean checkExistsHotel(Integer hotelId,String name);
 }
