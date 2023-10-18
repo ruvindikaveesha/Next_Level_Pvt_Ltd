0a9ff7caa6053eb81f05345e10bc7ebdf5bd631e
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 14:42:33 +0530

    update

---
+++ Driver_Service/src/test/java/com/example/driver_service/DriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.driver_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
