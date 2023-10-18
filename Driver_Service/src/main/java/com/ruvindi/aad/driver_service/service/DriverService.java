a58f6baa49e78f406f448073a1c32d48414f09c1
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 17:01:11 +0530

    update

--- Driver_Service/src/main/java/com/ruvindi/aad/driver_service/service/DriverService.java
+++ Driver_Service/src/main/java/com/ruvindi/aad/driver_service/service/DriverService.java
@@ -1,4 +1,16 @@
 package com.ruvindi.aad.driver_service.service;
 
+import com.ruvindi.aad.driver_service.entity.Driver;
+
+import java.util.List;
+
 public interface DriverService {
+    void addDriver (Driver driver);
+
+    List<Driver> fetchAllDriver();
+
+    boolean checkExistsDriver(Integer driverId, String name);
+
+    void  updateDriver(Driver driver);
+
 }
