cd34ed36c46e0e95125913ae5933910b92e73feb
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 22:42:08 +0530

    update

--- Travel_Service/src/main/java/com/ruvindi/aad/travel_service/controller/TravelPackageController.java
+++ Travel_Service/src/main/java/com/ruvindi/aad/travel_service/controller/TravelPackageController.java
@@ -17,19 +17,19 @@ public class TravelPackageController {
     @PostMapping(value = "/add_package")
     public Response addTravelPackage(@RequestBody TravelPackage travelPackage){
         travelPackageService.addTravelPackage(travelPackage);
-        return new Response("Okay","Added","null");
+        return new Response("Okay","Added",null);
     }
 
-    @PostMapping(value = "/update_package")
+    @PutMapping(value = "/update_package")
     public Response updateTravelPackage(@RequestBody TravelPackage travelPackage){
         travelPackageService.updateTravelPackage(travelPackage);
-        return new Response("Okay","Done","null");
+        return new Response("Okay","Done",null);
     }
 
-    @PostMapping(value = "/delete_package")
+    @DeleteMapping(value = "/delete_package")
     public Response deletePackage(@RequestParam Integer id){
         travelPackageService.deletePackage(id);
-        return new Response("Okay","Deleted","null");
+        return new Response("Okay","Deleted",null);
     }
 
     @GetMapping(value = "/get_packages")
