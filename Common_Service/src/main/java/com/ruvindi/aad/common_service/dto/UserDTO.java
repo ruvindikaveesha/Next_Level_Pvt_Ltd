2f152b61bd334a717fb3bb73dc80a893c0677819
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-18 22:25:01 +0530

    update

--- Common_Service/src/main/java/com/ruvindi/aad/common_service/dto/UserDTO.java
+++ Common_Service/src/main/java/com/ruvindi/aad/common_service/dto/UserDTO.java
@@ -1,4 +1,20 @@
 package com.ruvindi.aad.common_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserDTO {
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+    private String password;
+    private String userName;
+    private String nicOrPassportNum;
+    private String proPicUrl;
+    private String role;
 }
