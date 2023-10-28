a9634c231a989421444f7655de23a9bb3e68ff98
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-28 23:26:53 +0530

    update

---
+++ Server_Registry/src/test/java/com/ruvindi/aad/server_registry/ServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruvindi.aad.server_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
