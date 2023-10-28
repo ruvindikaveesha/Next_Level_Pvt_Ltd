453b948e10a726590f4d804c37339e0d4f3cf962
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-28 18:41:38 +0530

    update

---
+++ Cloud_getway/src/test/java/com/ruvindi/aad/cloud_getway/CloudGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruvindi.aad.cloud_getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
