385d912a00c220d8ac6f892e71b29e0c52c74b57
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-28 19:29:24 +0530

    update

--- User_Service/src/main/java/com/ruvindi/aad/user_service/dto/LoginDto.java
+++ User_Service/src/main/java/com/ruvindi/aad/user_service/dto/LoginDto.java
@@ -1,4 +1,12 @@
 package com.ruvindi.aad.user_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class LoginDto {
+
 }
--- User_Service/src/main/java/com/ruvindi/aad/user_service/util/Response.java
+++ User_Service/src/main/java/com/ruvindi/aad/user_service/util/Response.java
@@ -1,4 +1,17 @@
 package com.ruvindi.aad.user_service.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class Response {
+    private String state;
+    public String message;
+    private Object data;
+
 }
