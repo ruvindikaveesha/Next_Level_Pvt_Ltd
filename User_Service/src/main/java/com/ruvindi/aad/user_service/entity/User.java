e556cc85ef109927249d2e4a6935f20e218b3391
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-28 18:57:33 +0530

    update

--- User_Service/src/main/java/com/ruvindi/aad/user_service/entity/User.java
+++ User_Service/src/main/java/com/ruvindi/aad/user_service/entity/User.java
@@ -1,4 +1,27 @@
 package com.ruvindi.aad.user_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+    private String userName;
+    private String password;
+    private String nicOrPassportNum;
+    private String role;
+    private String proPicUrl;
+
 }
