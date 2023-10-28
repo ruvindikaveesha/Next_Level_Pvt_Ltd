9cb7b31957dd7745e035cb8570c7b86b3af92806
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-10-28 19:01:28 +0530

    update

--- User_Service/src/main/java/com/ruvindi/aad/user_service/repo/UserRepo.java
+++ User_Service/src/main/java/com/ruvindi/aad/user_service/repo/UserRepo.java
@@ -1,4 +1,15 @@
 package com.ruvindi.aad.user_service.repo;
 
-public interface UserRepo {
+import com.ruvindi.aad.user_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Integer> {
+
+    Boolean existsByUserName(String userName);
+    Boolean existsByUserNameAndPassword(String userName,String Password);
+
+    User findByUserNameAndPassword(String userName,String Password);
+
 }
